b2a7a61bc5e3576f4b4b85ffadc479fb772c6b90
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2020-09-15 15:25:06 -0500

    Completed project

---
+++ BradyStringVariableDemo/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace BradyStringVariableDemo
+{
+    partial class StringVariableDemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(85, 243);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(131, 49);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Show &Name";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(237, 243);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(131, 49);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(84, 59);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(144, 17);
+            this.lblFirstName.TabIndex = 2;
+            this.lblFirstName.Text = "Enter your first name:";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(82, 116);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(143, 17);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Enter your last name:";
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Location = new System.Drawing.Point(84, 166);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(117, 17);
+            this.lblFullName.TabIndex = 4;
+            this.lblFullName.Text = "Your full name is:";
+            this.lblFullName.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(265, 166);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput.TabIndex = 5;
+            this.lblOutput.Click += new System.EventHandler(this.LblOutput_Click);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(268, 111);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 22);
+            this.txtLastName.TabIndex = 7;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(268, 54);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtFirstName.TabIndex = 8;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.TxtFirstName_TextChanged);
+            // 
+            // StringVariableDemoForm
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(483, 337);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblFullName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "StringVariableDemoForm";
+            this.Text = "StringVariableDemoForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+    }
+}
+
---
+++ BradyStringVariableDemo/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/***************************************************************
+* Name        : String Variable Demo
+* Author      : Kabrina Brady
+* Created     : 9/7/2019
+* Course      : CIS 169 - C#
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work
+* Description : User types first and last name into TextBoxes and Submit button prints them in lblOutput with the first letters of the first and last names capitalized
+*               Input:  First name, last name
+*               Output: First name, space, and last name
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or unmodified. I have not given other fellow student(s) access to my program.         
+***************************************************************/
+
+namespace BradyStringVariableDemo
+{
+    public partial class StringVariableDemoForm : Form
+    {
+        public StringVariableDemoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblOutput_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = ""; //Sets the text of lblOutput to nothing
+        }
+        
+        //Exit button terminates program
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //When Submit button clicked, concatenates txtFirstName and txtLastName and displays in lblOutput and puts a space between the words
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            string varFirstName = txtFirstName.Text; //asigns user input to variable
+            string varLastName = txtLastName.Text; //asigns user input to variable
+            lblOutput.Text = char.ToUpper(varFirstName[0]) + varFirstName.Substring(1) + " " + 
+            char.ToUpper(varLastName[0]) + varLastName.Substring(1); //displays the user input and uppercases the first letters
+            //of the first and last names
+        }
+
+        private void TxtFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
